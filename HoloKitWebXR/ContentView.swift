fbf198bc8954ee12aebcee4f25f907491890ed2a
yuchen <yuchenz27@outlook.com>
2023-08-02 13:31:29 +0800

    Initial Commit

---
+++ HoloKitWebXR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HoloKitWebXR
+//
+//  Created by Yuchen Zhang on 2023/8/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HoloKitWebXR/HoloKitWebXRApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoloKitWebXRApp.swift
+//  HoloKitWebXR
+//
+//  Created by Yuchen Zhang on 2023/8/2.
+//
+
+import SwiftUI
+
+@main
+struct HoloKitWebXRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
